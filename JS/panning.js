194094423a7f74d9d035de852b5def122263b94b
Colin Theimer <colin.v.theimer@stud.hs-kempten.de>
2024-12-11 13:13:05 +0100

    panning adjustment

--- JS/panning.js
+++ JS/panning.js
@@ -1,10 +1,13 @@
 if (window.DeviceOrientationEvent) {
     window.addEventListener('deviceorientation', (event) => {
       const gamma = event.gamma;
+      const beta = event.beta; 
 
-      const positionX = Math.min(Math.max((gamma + 90) / 180, 0), 1) * 100;
+      const positionX = 100 - Math.min(Math.max((gamma + 90) / 180, 0), 1) * 100;
 
-      document.querySelector('.background-image').style.backgroundPosition = `${positionX}% center`;
+      const positionY = 100 - Math.min(Math.max((beta - 45) / 90, 0), 1) * 100;
+
+      document.querySelector('.background-image').style.backgroundPosition = `${positionX}% ${positionY}%`;
     });
   } else {
     console.warn('DeviceOrientationEvent is not supported on this device.');
